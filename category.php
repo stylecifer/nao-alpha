41670ac738d65eb59c4b40b328957ac4e3a40b70
esTonypro <antoniodigrafico@gmail.com>
2014-02-28 18:21:44 -0300

    Modificaciones de Os
    complete las modificaciones mínimas para el envío de la campaña por
    correo electrónico

--- category.php
+++ category.php
@@ -15,19 +15,6 @@ get_header(); ?>
 		<div id="content" class="site-content" role="main">
 
 			<?php if ( have_posts() ) : ?>
-
-			<header class="archive-header">
-				<h1 class="archive-title"><?php printf( __( 'Category Archives: %s', 'twentyfourteen' ), single_cat_title( '', false ) ); ?></h1>
-
-				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
-				?>
-			</header><!-- .archive-header -->
-
 			<?php
 					// Start the Loop.
 					while ( have_posts() ) : the_post();
@@ -37,7 +24,7 @@ get_header(); ?>
 					 * use this in a child theme, then include a file called called content-___.php
 					 * (where ___ is the post format) and that will be used instead.
 					 */
-					get_template_part( 'content', get_post_format() );
+					get_template_part( 'content-nao', get_post_format() );
 
 					endwhile;
 					// Previous/next page navigation.
